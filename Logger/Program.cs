3ddb28fedce20d0c691b1e28cceed32eff02cbb4
bfristad <benjaminfristad@gmail.com>
2023-01-21 20:39:59 -0800

    added prerequsites

---
+++ Logger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+}
