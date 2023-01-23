6b42ccb37b9849139af23b39e87325a931c96d47
Joshua Merrell <jmerrell2@ewu.edu>
2023-01-23 15:47:36 -0800

    Add FileLogger implementation, tests, and update project properties

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,10 +1,27 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
 
-namespace Logger.Tests;
-
-[TestClass]
-public class LogFactoryTests
+namespace Logger.Tests
 {
-    
+    [TestClass]
+    public class LogFactoryTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Error_WithNoConfiguration_ReturnsNull()
+        {
+            LogFactory factory = new LogFactory();
+            BaseLogger log = factory.CreateLogger("FileLogger");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Error_WithUnrecognizedClassname_ThrowsArgumentException()
+        {
+            LogFactory factory = new LogFactory();
+            factory.ConfigureLogger("log.log");
+            BaseLogger log = factory.CreateLogger("NonexistentLogger");
+        }
+    }
 }
