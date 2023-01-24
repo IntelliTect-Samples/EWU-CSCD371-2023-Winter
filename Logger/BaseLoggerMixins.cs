22fe73d86f238beb6f02d4adb438810a0ed1abc1
Joshua Hess <82059700+j0shh3ss@users.noreply.github.com>
2023-01-24 14:35:02 -0800

    Update BaseLoggerMixins.cs
    Update

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -7,29 +7,29 @@ namespace Logger
         public static void Error(this BaseLogger logger, string message, params object[] arr)
         {
             if (logger is null) { throw new ArgumentNullException(logger!.ToString()); }
-            if (message == null) { throw new ArgumentNullException(message); }
-            if (arr == null) { throw new ArgumentNullException("params is null in error"); }
+            if (message is null) { throw new ArgumentNullException(message); }
+            if (arr is null) { throw new ArgumentNullException("params is null in error"); }
             logger.Log(LogLevel.Error, string.Format(message, arr));
         }
         public static void Warning(this BaseLogger logger, string message, params object[] arr)
         {
             if (logger is null) { throw new ArgumentNullException(logger!.ToString()); }
-            if (message == null) { throw new ArgumentNullException(message); }
-            if (arr == null) { throw new ArgumentNullException("params is null in warning"); }
+            if (message is null) { throw new ArgumentNullException(message); }
+            if (arr is null) { throw new ArgumentNullException("params is null in warning"); }
             logger.Log(LogLevel.Warning, string.Format(message, arr));
         }
         public static void Information(this BaseLogger logger, string message, params object[] arr)
         {
             if (logger is null) { throw new ArgumentNullException(logger!.ToString()); }
-            if (message == null) { throw new ArgumentNullException(message); }
-            if (arr == null) { throw new ArgumentNullException("params is null in info"); }
+            if (message is null) { throw new ArgumentNullException(message); }
+            if (arr is null) { throw new ArgumentNullException("params is null in info"); }
             logger.Log(LogLevel.Information, string.Format(message, arr));
         }
         public static void Debug(this BaseLogger logger, string message, params object[] arr)
         {
             if (logger is null) { throw new ArgumentNullException(logger!.ToString()); }
-            if (message == null) { throw new ArgumentNullException(message); }
-            if (arr == null) { throw new ArgumentNullException("params is null in debug"); }
+            if (message is null) { throw new ArgumentNullException(message); }
+            if (arr is null) { throw new ArgumentNullException("params is null in debug"); }
             logger.Log(LogLevel.Debug, string.Format(message, arr));
         }
     }
