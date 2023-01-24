ff2d72e491949eaa658b3a0e207db230b57784f4
Nolan Posey <nposey1@ewu.edu>
2023-01-23 16:02:56 -0800

    BaseLoggerMixins throws ArgumentNullException if log is null
    Created test for FileLogger FileLogger_IsNotNull_ReturnsTrue().

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -5,5 +5,18 @@ namespace Logger.Tests;
 [TestClass]
 public class FileLoggerTests
 {
-    
+    [TestMethod]
+    public void FileLogger_IsNotNull_ReturnsTrue()
+    {
+        //Arrange
+        LogFactory factory = new LogFactory();
+        factory.ConfigureLogger("FileLoggerTest.log");
+        BaseLogger fileLogger = factory.CreateLogger("FileLogger");
+
+        //Act
+        fileLogger.Log(LogLevel.Error, "Test Error Message");
+
+        //Assert
+        Assert.IsNotNull(fileLogger);
+    }
 }
--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -7,19 +7,38 @@ public static class BaseLoggerMixins
 {
     public static void Error(this BaseLogger log, string message, params object[] args)
     {
-        if(log is null)
+        if (log is null)
+        {
             throw new ArgumentNullException("log must not be null");
+        }
         log.Log(LogLevel.Error, String.Format(message, args));
     }
 
     public static void Warning(this BaseLogger log, string message, params object[] args)
     {
+        if (log is null) 
+        {
+            throw new ArgumentNullException("log must not be null");
+        }
         log.Log(LogLevel.Warning, $"{message} {args}");
     }
 
     public static void Information(this BaseLogger log, string message, params object[] args)
     {
+        if(log is null)
+        {
+            throw new ArgumentNullException("log must not be null");
+        }
         log.Log(LogLevel.Information, $"{message} {args}");
+    }
+
+    public static void Debug(this BaseLogger log, string message, params object[] args) 
+    {
+        if(log is null)
+        {
+            throw new ArgumentNullException("log must not be null");
+        }
+        log.Log(LogLevel.Debug, $"{message} {args}");
     }
 }
 
