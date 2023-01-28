dedd24aab95ac1acf1b86263db7dd85e709903c0
Helaman99 <80353517+arose-13@users.noreply.github.com>
2023-01-28 13:25:29 -0800

    Added Tests for Jester and JokeService

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -5,6 +5,37 @@ namespace CanHazFunny.Tests
     [TestClass]
     public class JesterTests
     {
-        
+        [TestMethod]
+        public void CreateJester_ReturnsJester()
+        {
+            // Assemble
+            JokeService service = new JokeService();
+            Jester jester = new Jester(service);
+
+            // Assert
+            Assert.IsNotNull(jester);
+        }
+
+        [TestMethod]
+        public void CreateJester_SuccessfullyGetJoke()
+        {
+            // Assemble
+            Jester jester = new Jester(new JokeService());
+
+            // Act
+            string joke = jester.Service.GetJoke();
+
+            // Assert
+            Assert.IsNotNull(joke);
+            Assert.AreNotEqual(joke, "");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void Create_GivenNullService_ThrowsException()
+        {
+            // Assemble - Act
+            Jester jester = new Jester(null);
+        }
     }
 }
---
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CanHazFunny.Tests
+{
+    [TestClass]
+    public class JokeServiceTests
+    {
+        [TestMethod]
+        public void CreateJokeService_ReturnsJokeService()
+        {
+            // Assemble
+            JokeService service = new JokeService();
+
+            // Assert
+            Assert.IsNotNull(service);
+        }
+
+        [TestMethod]
+        public void CreateJokeService_SuccessfullyGetJoke()
+        {
+            // Assemble
+            JokeService service = new JokeService();
+
+            // Act
+            string joke = service.GetJoke();
+
+            // Assert
+            Assert.IsNotNull(joke);
+            Assert.AreNotEqual(joke, "");
+        }
+    }
+}
