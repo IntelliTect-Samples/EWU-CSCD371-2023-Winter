d2a266d2f0549544be358f9170b3ee9e730cce91
Bhopss152 <bhopkins2@ewu.edu>
2023-01-29 19:51:01 -0800

    Introducing tests

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,10 +1,21 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace CanHazFunny.Tests
 {
     [TestClass]
     public class JesterTests
     {
-        
+        [TestMethod]
+        public void Jester_Has_JokeService()
+        {
+            //Arrange
+            IJokeService jokeService = new JokeService();
+            IJokeOutput jokeOut = new JokeOutput();
+            //Act
+            Jester jester = new(jokeService, jokeOut);
+            //Assert
+            Assert.AreEqual<IJokeService>(jester.JokeService, jokeService);
+        }
     }
 }
