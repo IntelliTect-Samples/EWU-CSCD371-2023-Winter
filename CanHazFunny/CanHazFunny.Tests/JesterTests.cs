8b460889d4977e151532295444e50b94baba0661
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2023-01-30 21:25:14 -0800

    Starting on Unit Testing

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,10 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace CanHazFunny.Tests
 {
     [TestClass]
     public class JesterTests
     {
-        
+        [TestMethod]
+        [ExpectedException(ArgumentNullException)]
+        public void JesterInitialize_TestNullPrintService()
+        {
+            
+        }
     }
 }
