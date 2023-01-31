ca4f70d376c1b5760f919cb1e7ac33cebf12d1e9
Ian Kaiserman <ian.kaiserman@gmail.com>
2023-01-30 20:26:00 -0800

    Assignment 3 Submission

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,10 +1,61 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.IO;
 
-namespace CanHazFunny.Tests
+namespace CanHazFunny.Tests;
+
+[TestClass]
+public class JesterTests
 {
-    [TestClass]
-    public class JesterTests
+    [TestMethod]
+    public void TellJoke_GivenJoke_ReturnJoke()
     {
-        
+        // Arrange
+        var jokeServiceMock = new Mock<IJoke>();
+        JokeService jokeService = new();
+        jokeServiceMock.SetupSequence(jokeService => jokeService.GetJoke()).Returns("Test joke");
+        Jester testJester = new(jokeServiceMock.Object, new ConsoleOutput());
+
+        // Act
+        string joke = testJester.TellJoke();
+
+        // Assert
+        Assert.AreEqual<string>("Test joke", joke);
+    }
+
+    [TestMethod]
+    public void TellJoke_GivenChuckNorris_SkipJoke()
+    {
+        // Arrange
+        var jokeServiceMock = new Mock<IJoke>();
+        JokeService jokeService = new();
+        jokeServiceMock.SetupSequence(jokeService => jokeService.GetJoke())
+            .Returns("Chuck Norris")
+            .Returns("Test joke");
+        Jester testJester = new(jokeServiceMock.Object, new ConsoleOutput());
+
+        // Act
+        string joke = testJester.TellJoke();
+
+        // Assert
+        Assert.AreEqual<string>("Test joke", joke);
+    }
+
+    [TestMethod]
+    public void ConsoleOutput_Write_WritesLine()
+    {
+        // Arrange
+        string text = "test string";
+        ConsoleOutput consoleOutput = new();
+        StringWriter writer = new();
+        Console.SetOut(writer);
+
+        // Act
+        consoleOutput.Write(text);
+        string result = writer.ToString().Trim();
+
+        // Assert
+        Assert.AreEqual<string>(text, result);
     }
 }
---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny;
+
+public class Jester
+{
+    private IJoke? _Joke;
+    private IOutput? _Output;
+
+    public Jester(IJoke joke, IOutput output)
+    {
+        Joke = joke;
+        Output = output;
+    }
+
+    public IJoke Joke
+    {
+        get
+        {
+            return _Joke!;
+        }
+        set
+        {
+            ArgumentNullException.ThrowIfNull(nameof(value));
+            _Joke = value;
+        }
+    }
+
+    public IOutput Output
+    {
+        get
+        {
+            return _Output!;
+        }
+        set
+        {
+            ArgumentNullException.ThrowIfNull(nameof(value));
+            _Output = value;
+        }
+    }
+
+    public string TellJoke()
+    {
+        string joke = Joke.GetJoke();
+        while (joke.Contains("Chuck Norris"))
+        {
+            joke = Joke.GetJoke();
+        }
+        Output.Write(joke);
+        return joke;
+    }
+}
