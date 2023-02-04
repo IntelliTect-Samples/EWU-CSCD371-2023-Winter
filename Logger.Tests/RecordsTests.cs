c7425cd88db983f96ffefcf2e8fc04582ff9684e
Joshua Merrell <jmerrell2@ewu.edu>
2023-02-03 17:04:34 -0800

    Add records, BaseEntity, IEntity, and tests

---
+++ Logger.Tests/RecordsTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Tests;
+
+[TestClass]
+public class RecordsTests
+{
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Title_AssignNullInInitializer_ThrowException()
+    {
+        _ = new Book(null, "null");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Author_AssignNullInInitializer_ThrowException()
+    {
+        _ = new Book("null", null);
+    }
+
+    [TestMethod]
+    public void Book_Name_EqualsTitle()
+    {
+        Book book = new("Neuromancer", "William Gibson");
+        Assert.AreEqual<string>(book.Title, book.Name);
+        Assert.AreEqual<string>("Neuromancer", book.Name);
+    }
+
+    [TestMethod]
+    public void ID_NonNull() {
+        Book book = new("2001: A Space Odyssey", "Arthuer C Clarke");
+        Assert.IsNotNull(book.ID);
+    }
+
+    [TestMethod]
+    public void Student_NameChange_TestEqual()
+    {
+        Student student = new("Joe McSurname", "EWU")
+        {
+            Name = "Joe McLastname"
+        };
+        Assert.AreEqual<string>("Joe McLastname", student.Name);
+    }
+
+    [TestMethod]
+    public void Employee_NameChange_TestEqual()
+    {
+        Employee employee = new("Joe McSurname", "EWU")
+        {
+            Name = "Joe McLastname"
+        };
+        Assert.AreEqual("Joe McLastname", employee.Name);
+    }
+}
---
+++ Logger/Records.cs
@@ -0,0 +1,23 @@
+﻿namespace Logger;
+
+public abstract record class BaseRecord(string? Name) : IEntity
+{
+    public Guid ID { get; init; } = Guid.NewGuid();
+    public string Name { get; set; } = Name ?? throw new ArgumentNullException(nameof(Name));
+}
+
+public record class Book(string? Title, string? Author) : BaseRecord(Title)
+{
+    public string Title { get; set; } = Title ?? throw new ArgumentNullException(nameof(Title));
+    public string Author { get; set; } = Author ?? throw new ArgumentNullException(nameof(Author));
+}
+
+public record class Student(string? Name, string? School) : BaseRecord(Name)
+{
+    public string School { get; set; } = School ?? throw new ArgumentNullException(nameof(School));
+}
+
+public record class Employee(string? Name, string? Employer) : BaseRecord(Name)
+{
+    public string Employer { get; set; } = Employer ?? throw new ArgumentNullException(nameof(Employer));
+}
