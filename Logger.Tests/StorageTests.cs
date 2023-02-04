05a5c1dd1f631ec812ae67afed760e4636229b8d
Nolan Posey <nposey1@ewu.edu>
2023-02-04 15:30:40 -0800

    Added 4 tests to StorageTests()

--- Logger.Tests/StorageTests.cs
+++ Logger.Tests/StorageTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,78 @@ using System.Threading.Tasks;
 
 namespace Logger.Tests
 {
-    internal class StorageTests
+    [TestClass]
+    public class StorageTests
     {
+        private Storage? TestStorage;
+        private Book testBook = new("The Dude Book", "Mr.Man");
+        private Student testStudent = new("John", "EWU");
+        private Employee testEmployee = new("Jane", "Intellitect");
+        [TestInitialize] 
+        public void Init() 
+        { 
+            TestStorage = new Storage();
+            TestStorage!.Add(testBook);
+            TestStorage!.Add(testStudent);
+            TestStorage!.Add(testEmployee);
+        }
+
+        [TestMethod]
+        public void Storage_AddsEntityBook_ContainsBookTrue()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.IsTrue(TestStorage!.Contains(testBook));
+        }
+
+        [TestMethod]
+        public void Storage_AddsEntityStudent_ContainsStudentTrue()
+        {
+            //Arrange
+            
+            //Act
+
+            //Assert
+            Assert.IsTrue(TestStorage!.Contains(testStudent));
+        }
+
+        [TestMethod]
+        public void Storage_AddsEntityEmployee_ContainsStudentTrue()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.IsTrue(TestStorage!.Contains(testEmployee));
+        }
+
+        [TestMethod]
+        public void Storage_RemovesEntity_ContainsReturnsFalse()
+        {
+            //Arrange
+
+            //Act
+            TestStorage!.Remove(testEmployee);
+
+            //Assert
+            Assert.IsFalse(TestStorage.Contains(testEmployee));
+        }
+
+        /*[TestMethod]
+        public void Storage_GivenEntityID_GetReturnsSameEntity()
+        {
+            //Arrange
+
+            //Act
+            IEntity sameBook = TestStorage!.Get(testBook.ID)!;
+
+            //Assert
+            Assert.AreEqual<string>(testBook.Name,sameBook.Name);
+        }*/
+
     }
 }
