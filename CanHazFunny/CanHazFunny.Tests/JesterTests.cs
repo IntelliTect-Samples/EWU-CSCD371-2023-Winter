33ec64d4e1f936458d36d0f8490d924b795e39b8
Nolan Posey <nposey1@ewu.edu>
2023-02-03 16:49:09 -0800

    Solving merge conflict

---
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -0,0 +1,109 @@
+using CanHazFunny;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CanHazFunny.Tests
+{
+    [TestClass]
+    public class JesterTests
+    {
+        private Jester TestingJester;
+        [TestInitialize] 
+        public void Initialize() 
+        {
+            TestingJester = new Jester(new MockJokeOutput(), new MockJokeService());
+        }
+
+        [TestMethod]
+        public void Jester_CreateSuccessfully()
+        {   
+            //Act
+
+            //Assert
+            Assert.IsNotNull(TestingJester);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Jester_NullOutput_ThrowsArgumentNullException()
+        {
+            // Arrange
+
+            // Act
+            TestingJester.Output = null;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Jester_NullService_ThrowsArgumentNullException()
+        {
+            // Arrange
+
+            // Act
+            TestingJester.Service = null;
+        }
+
+        [TestMethod]
+        public void Jester_GetJoke_ReturnsJoke()
+        {
+            //Arrange
+
+            //Act
+            string joke = TestingJester.GetJoke();
+
+            //Assert
+            Assert.AreNotEqual<string>("", joke);
+        }
+
+        [TestMethod]
+        public void Jester_TellJoke_Returns()
+        {
+            //Arrange
+
+            //Act
+            TestingJester.TellJoke();
+
+            //Assert
+            Assert.IsTrue(((MockJokeOutput)TestingJester.Output).ToldJoke);
+        }
+
+        [TestMethod]
+        public void Jester_NewJokeDoesNotContainChuckNorris_ReturnsFalse()
+        {
+            //Arrange
+
+            //Act
+            string joke = TestingJester.TellJoke();
+
+            //Assert
+            Assert.IsFalse(joke.Contains("Chuck Norris"));
+        }
+    }
+}
+
+class MockJokeService : IJokeService
+{
+    private int NumRequests;
+    public string GetJoke()
+    {
+        string joke = NumRequests switch
+        {
+            0 => "Chuck Norris",
+            1 => "haha Chuck Norris xD",
+            2 => "Something something Chuck Norris",
+            3 => "yet another Chuck Norris joke",
+            _ => "joke",
+        };
+        NumRequests++;
+        return joke;
+    }
+}
+
+class MockJokeOutput : IJokeOutput
+{
+    public bool ToldJoke;
+    public void TellJoke(string joke)
+    {
+        ToldJoke = true;
+    }
+}
