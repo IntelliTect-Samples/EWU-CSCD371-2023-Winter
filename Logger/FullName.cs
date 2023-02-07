71e95f004b3798406d94102c2803beef5afff5c3
Vera <vkorchemnaya@eagles.ewu.edu>
2023-02-06 17:41:41 -0800

    FullName record

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Logger
-{
+namespace Logger;
 
-    public record struct FullName
-    {
-    }
+
+public record struct FullName(string First, string Last, string Middle)
+{
+    public string First { get; } = First ?? throw new ArgumentNullException(nameof(First));
+    public string Last { get; } = Last ?? throw new ArgumentNullException(nameof(Last));
+    public string Middle { get; }
 }
