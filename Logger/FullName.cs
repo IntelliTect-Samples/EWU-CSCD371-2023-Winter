ab19e181d2ea4668a5732dc405aa6794eaea99bb
Vera <vkorchemnaya@eagles.ewu.edu>
2023-02-06 18:07:36 -0800

    Comments on FullName struct

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 namespace Logger;
 
 
-public record struct FullName(string First, string Last, string Middle)
+// We chose to use a struct because we wanted value-based comparison and because 
+// it is needed as a light-weight data type
+
+// We made the class readonly (immutable) because we wanted to avoid the confusion
+// that is associated with copying value types and dereferencing value types
+public readonly record struct FullName(string First, string Last, string? Middle)
 {
     public string First { get; } = First ?? throw new ArgumentNullException(nameof(First));
     public string Last { get; } = Last ?? throw new ArgumentNullException(nameof(Last));
-    public string Middle { get; }
+    public string Middle { get; } = Middle ?? string.Empty;
 }
