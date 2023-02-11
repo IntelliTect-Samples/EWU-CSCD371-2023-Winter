8a10eb23b8d7b25341aaaff5b906c01214b63249
Howard404 <Gh031800#>
2023-02-10 17:12:42 -0800

    Node.cs Next property implemented

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 public class Node<T>
 {
     public T Data { get; set; }
-    public Node<T> Next { get; set; }
+    public Node<T> Next { get => _Next; set => _Next = value ?? throw new ArgumentNullException(); }
+    private Node<T> _Next;
     public Node(T data)
     {
         Data = data;
-        Next = this;
+        _Next = this;
     }
 
     public override string ToString()
