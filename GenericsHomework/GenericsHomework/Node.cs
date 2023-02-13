22d1e2058e0b3ef47ca5ef057f30c932894da932
Bhopss152 <bhopkins2@ewu.edu>
2023-02-13 14:12:46 -0800

    Startup code

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomework.GenericsHomework;
+
+public class Node<T>
+{
+    public T? Data { get; set; }
+    private int Size { get; set; }
+    public Node<T> Next { get; set; }
+    public Node(T? data)
+    {
+        Data = data;
+    }
+    public override string ToString()
+    {
+        return Data?.ToString() ?? "null";
+    }
+    public void Append(T data)
+    {
+        Next = new Node<T>(data);
+    }
+    public bool existing(T data)
+    {
+        if(data is null)
+        {
+            throw new ArgumentNullException(nameof(data));
+        }
+        Node<T> cur = Next;
+        while(cur != null) 
+        {
+            if (cur.Data.Equals(data)) { return true; }
+            cur = cur.Next;
+        }
+        return false;
+
+    }
+}
