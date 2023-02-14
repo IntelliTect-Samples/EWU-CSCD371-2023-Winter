0781ca93744417a780fd5c5b4f814775f13429d2
BBartels1 <bbartels1@eagles.ewu.edu>
2023-02-13 20:17:27 -0800

    assignment5 full commit

---
+++ GenericsHomework.Tests/Node.Tests.cs
@@ -0,0 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GenericsHomework.Tests
+{
+    [TestClass]
+    public class NodeTest
+    {
+        [TestMethod]
+        public void Append_WithNotNullValue_AppendsNode()
+        {
+            Node<int> node = new(4);
+            node.Append(0);
+
+
+            Assert.IsTrue(node.Exists(0));
+        }
+
+        [TestMethod]
+        public void Append_WithNullValue_DoesNotAppend()
+        {
+            Node<string> node = new("testString");
+            node.Append(null!);
+
+            Assert.IsFalse(node.Exists(null!));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Append_WithDuplicateValue_ThrowsException()
+        {
+            Node<double> node = new(20.2);
+            node.Append(20.2);
+
+        }
+
+        [TestMethod]
+        public void Clear_RemovesAllButCurrentNode()
+        {
+            Node<char> node = new('a');
+            node.Append('b');
+            node.Append('c');
+            node.Append('d');
+            node.Clear();
+
+            Assert.IsTrue(node.Exists('a'));
+            Assert.IsFalse(node.Exists('b'));
+            Assert.IsFalse(node.Exists('c'));
+            Assert.IsFalse(node.Exists('d'));
+        }
+    }
+}
---
+++ GenericsHomework/Node.cs
@@ -0,0 +1,62 @@
+﻿namespace GenericsHomework
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Next { get; private set; } 
+
+        public Node(T value) 
+        {
+            Value = value;
+            Next = this;
+        }
+
+        public void Append(T value)
+        {
+            if(value is not null) 
+            {
+                Node<T> cur = this;
+                do
+                {
+                    if (value.Equals(cur.Value))
+                    {
+                        throw new Exception("value already exists");
+                    }
+                    cur = cur.Next;
+                }
+                while (cur != this);
+
+                Node<T> next = new(value);
+                next.Next = this;
+                Next = next;     
+            }            
+        }
+
+        public void Clear() 
+        {
+            Next = this;
+        }
+
+        public bool Exists(T value)
+        {
+            if(value is null) return false;
+            Node<T> cur = this;
+            do
+            {
+                if (value.Equals(cur.Value))
+                {
+                    return true;
+                }
+                cur = cur.Next;
+            }
+            while (cur != this);
+            return false;
+        }
+
+        public override string? ToString()
+        {
+            if(Value is null) return null;
+            return Value.ToString();  
+        }
+    }
+}
