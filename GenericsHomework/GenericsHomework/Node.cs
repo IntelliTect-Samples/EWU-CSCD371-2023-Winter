715d4199887318cef0b4625b5e118c80d41c95fa
Gunnar Knutson <gknutson1@ewu.edu>
2023-02-13 22:56:25 -0800

    implement System.Collections.Generic.ICollection

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,8 +1,9 @@
-﻿using System.Data;
+﻿using System.Collections;
+using System.Data;
 
 namespace GenericsHomework;
 
-public class Node<T>
+public class Node<T> : ICollection<T>
 {
     private Node<T>? _next;
 
@@ -20,11 +21,20 @@ public class Node<T>
         private set => _next = value;
     }
 
-    public T Item { get; }
-    
-    public void Append(T item) => Next = !Exists(item) ? new Node<T>(item, Next) : throw new DuplicateNameException();
+    public T Item { get; private set; }
 
-    public override string? ToString() => Item?.ToString();
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(T item) => Append(item);
+
 
     // We don't need to worry about a circular loop
     // because we set `Next` to `this` for every `Node`, breaking the loop
@@ -33,6 +43,64 @@ public class Node<T>
         var node = Next;
         Next = this;
         if (node != this) Next.Clear();
+    }
+
+    public bool Contains(T item) => Exists(item);
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        if (array == null) throw new ArgumentNullException(nameof(array));
+        if (arrayIndex < 0) throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+        var node = this;
+        do
+        {
+            array[arrayIndex++] = node.Item;
+            node = node.Next;
+        } while (node.Next != node);
+    }
+
+    public bool Remove(T item)
+    {
+        var node = this;
+        while (true)
+        {
+            if (Equals(node.Item, item))
+            {
+                node.Item = node.Next.Item;
+                node.Next = node.Next;
+                return true;
+            }
+
+            if (node.Next == node) return false;
+        }
+    }
+
+    public int Count
+    {
+        get
+        {
+            int i = 1;
+            var node = this;
+            while (node.Next != node)
+            {
+                node = node.Next;
+                i++;
+            }
+
+            return i;
+        }
+    }
+
+    public bool IsReadOnly => false;
+
+    public void Append(T item)
+    {
+        Next = !Exists(item) ? new Node<T>(item, Next) : throw new DuplicateNameException();
+    }
+
+    public override string? ToString()
+    {
+        return Item?.ToString();
     }
 
     public bool Exists(T item) => Equals(item, Item) || (Next != this && Next.Exists(item));
