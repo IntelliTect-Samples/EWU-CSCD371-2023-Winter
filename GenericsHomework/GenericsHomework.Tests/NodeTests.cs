26a81d9eac8be01e0362dc4ca9d5f45946e274c3
daisypalencia <dpalencia@ewu.edu>
2023-02-13 21:51:05 -0800

    changes made to for assignment5

---
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericsHomework.Tests;
+
+[TestClass]
+public class NodeTests
+{
+    [TestMethod]
+    public void ToStringValuePass()
+    {
+        int value = 60;
+        Node<int> test = new(value);
+
+        Assert.AreEqual<string>(value.ToString(), test.ToString());
+
+    }
+    [TestMethod]
+    public void NodeStringPass()
+    {
+        string value = "node";
+        Node<string> test = new(value);
+
+        Assert.AreEqual<string>(value, test.ToString());
+
+    }
+}
---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace GenericsHomework
+{
+    public class Node<T>
+    {
+        public T Value { get; }
+
+        public Node<T> Next { get; private set; }
+        public Node(T value)
+        {
+            Value = value;
+            Next = this;
+        }
+
+        public override string ToString()
+        {
+            return Value?.ToString() ?? "null";
+        }
+
+        public Node<T> Append(T value)
+        {
+            if (value != null)
+            {
+                Next.Append(value);
+            }
+            return this;
+        }
+
+        public void Clear()
+        {
+            if (Next == this)
+            {
+                return;
+            }
+            var next = Next;
+            Next = this;
+            next.Clear();
+        }
+
+        public bool Exists(T value)
+        {
+            var currentNode = this;
+            while (currentNode != null && value != null)
+            {
+                if (currentNode.Equals(value))
+                {
+                    return true;
+                }
+                else if (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+            }
+            return false;
+        }
+
+    }
+}
+
