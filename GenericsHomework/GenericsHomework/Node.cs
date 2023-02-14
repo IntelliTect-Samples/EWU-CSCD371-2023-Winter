71422b86ca0a147fc4f7ccd694ab30fefec3e5e4
George <georgekoralski@hotmail.com>
2023-02-13 19:21:34 -0800

    Generic Implementation of Node<T> and Tests

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,54 @@
+﻿namespace GenericsHomework;
+
+public class Node<T>
+{
+    public T Value { get; set; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T value)
+    {
+        Value = value;
+        Next = this;
+    }
+
+    public override string ToString()
+    {
+        return Value?.ToString() ?? "null";
+    }
+
+    public void Clear()
+    {
+        Node<T> current = this;
+        do
+        {
+            Node<T> next = current.Next;
+            current.Next = current;
+            current = next;
+        } while (current != this);
+    }
+
+    public void Append(T value)
+    {
+        if (Exists(value))
+            throw new ArgumentException("Value already exists in the list", nameof(value));
+
+        // Create a new node and insert it between this and this.Next
+        Node<T> newNode = new Node<T>(value)
+        {
+            Next = this.Next
+        };
+        this.Next = newNode;
+    }
+
+    public bool Exists(T value)
+    {
+        Node<T> current = this;
+        do
+        {
+            if (object.Equals(value, current.Value))
+                return true;
+            current = current.Next;
+        } while (current != this);
+        return false;
+    }
+}
---
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -0,0 +1,79 @@
+﻿using GenericsHomework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericsHomework.Tests
+{
+    [TestClass]
+    public class NodeTests
+    {
+        [TestMethod]
+        public void Node_Constuctor_Success()
+        {
+            Node<int> node = new Node<int>(10);
+            Assert.IsNotNull(node);
+            Assert.AreEqual(node, node.Next);
+        }
+
+        [TestMethod]
+        public void Node_ToString_Success()
+        {
+            string testValue = "Test String";
+            Node<string> node = new Node<string>(testValue);
+            Assert.AreEqual(testValue, node.ToString());
+        }
+
+        [TestMethod]
+        public void Node_ClearOneNode_Success()
+        {
+            Node<int> node = new(15);
+            node.Clear();
+            Assert.AreEqual(node, node.Next);
+        }
+
+        [TestMethod]
+        public void Node_ClearTwoNodes_Success()
+        {
+            Node<int> node = new(1);
+            node.Append(2); // Create a second node
+            var node2 = node.Next;
+
+            node.Clear();
+            // Check if all nodes point to themself
+            Assert.AreEqual(node, node.Next);
+            Assert.AreEqual(node2, node2.Next);
+        }
+
+        [TestMethod]
+        public void Node_Append_Success()
+        {
+            Node<int> node = new(1);
+            node.Append(2); // Create a second node
+            var node2 = node.Next;
+
+            Assert.AreEqual(node2, node.Next);
+            Assert.AreEqual(node, node2.Next);
+        }
+
+        [TestMethod]
+        public void Node_AppendValueExist_Exception()
+        {
+            Node<int> node = new(1);
+            node.Append(2); // Create a second node
+            var node2 = node.Next;
+            Assert.ThrowsException<ArgumentException>(()=>node2.Append(1),
+                "Value already exists in the list");
+        }
+
+        [TestMethod]
+        public void Node_Exists_Success()
+        {
+            Node<int> node = new(1);
+            node.Append(2); // Create a second node
+            var node2 = node.Next;
+
+            Assert.IsTrue(node.Exists(1));
+            Assert.IsTrue(node.Exists(2));
+            Assert.IsFalse(node.Exists(3));
+        }
+    }
+}
