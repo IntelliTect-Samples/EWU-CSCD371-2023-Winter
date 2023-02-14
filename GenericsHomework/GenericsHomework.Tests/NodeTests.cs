a7c6ee121c32537371e6dcad587d40a8c32b4492
BenFristadSchool <bfristad@ewu.edu>
2023-02-13 20:04:55 -0800

    finished unit tests

--- GenericsHomework/GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -1,14 +1,71 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
 
 namespace GenericsHomework.Tests
 {
     [TestClass]
-    public class NodesTests
+    public class NodeTests
     {
+        Node<int> node = new(1, null!);
+
         [TestMethod]
-        public void TestMethod1()
+        public void CreateNode_ReturnsNode()
         {
+            // Assert
+            Assert.AreEqual<Node<int>>(node, node.Next);
+            Assert.AreEqual<int>(node.Value, 1);
+        }
+
+        [TestMethod]
+        public void ToString_ReturnsString()
+        {
+            // Act
+            string expected = "1";
+
+            // Assert
+            Assert.AreEqual<string>(expected, node.ToString());
+        }
+
+        [TestMethod]
+        public void Append_Success()
+        {
+            node.Append(2);
+            Assert.AreEqual<Node<int>>(node, node.Next.Next);
+            Assert.AreEqual<int>(node.Next.Value, 2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Append_Failure()
+        {
+            node.Append(2);
+            node.Append(2);
+        }
+
+        [TestMethod]
+        public void Clear_CutsAllLinks()
+        {
+            // Act
+            node.Append(2);
+            node.Append(3);
+            node.Clear();
+
+            // Assert
+            Assert.AreEqual<Node<int>>(node, node.Next);
+        }
+
+        [TestMethod]
+        public void Exists_ReturnsTrue()
+        {
+            node.Append(2);
+            Assert.IsTrue(node.Exists(2));
+        }
+
+        [TestMethod]
+        public void Exists_ReturnsFalse()
+        {
+            node.Append(2);
+            Assert.IsFalse(node.Exists(3));
         }
     }
 }
--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,7 +1,63 @@
 ﻿namespace GenericsHomework
 {
-    public class Node
+    public class Node<T>
     {
+        public T Value { get; set; }
+        public Node<T> Next { get; private set; }   
 
+        public Node(T value, Node<T> next)
+        {
+            Value = value;
+            Next = next ?? this;
+        }
+
+        public override string ToString()
+        {
+            return Value!.ToString() ?? "value is null";
+        }
+
+        public void Append(T newValue)
+        {
+            Next = !Next.Exists(newValue) ? new Node<T>(newValue, Next) : throw new ArgumentException("value already exists in linked list");
+        }
+
+
+        /*
+         * WHY THIS CLEAR FUNCTION WORKS
+         * 
+         * It is sufficient to set next to itself becuase it will cut all off all the connected links
+         * 
+         * The removed items should not be set to circle back on themselves because it is a waste of a process.
+         * Once the list of nodes are not referenced they will be collected by the garbage collector
+         * 
+         * I believe you would need to worry if you connected the linked list after removing your reference to that list
+         * because every node would be referenced by another node, which I belive would create a memory leak because the 
+         * garabge collector would not remove the abandoned nodes since they are all being referenced by eachother.
+         */
+        public void Clear()
+        {
+            Next = this;
+        }
+
+        public bool Exists(T value)
+        {
+            Node<T> curr = Next;
+            while(!curr.Equals(this))
+            {
+                if(curr.Value is not null && curr.Value.Equals(value))
+                {
+                    return true;
+                }
+                else
+                {
+                    curr = curr.Next;
+                }
+            }
+            if (curr.Value is not null && curr.Value.Equals(value))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
