092c08d2cb9759d8869092694bdf46d280cdedb4
Gunnar Knutson <gknutson1@ewu.edu>
2023-02-13 22:25:23 -0800

    Homework 5 Complete

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+
+namespace GenericsHomework;
+
+public class Node<T>
+{
+    private Node<T>? _next;
+
+    public Node(T item) => Item = item;
+
+    private Node(T item, Node<T> next)
+    {
+        Item = item;
+        Next = next;
+    }
+
+    public Node<T> Next
+    {
+        get => _next ?? this;
+        private set => _next = value;
+    }
+
+    public T Item { get; }
+    
+    public void Append(T item) => Next = !Exists(item) ? new Node<T>(item, Next) : throw new DuplicateNameException();
+
+    public override string? ToString() => Item?.ToString();
+
+    // We don't need to worry about a circular loop
+    // because we set `Next` to `this` for every `Node`, breaking the loop
+    public void Clear()
+    {
+        var node = Next;
+        Next = this;
+        if (node != this) Next.Clear();
+    }
+
+    public bool Exists(T item) => Equals(item, Item) || (Next != this && Next.Exists(item));
+}
---
+++ GenericsHomework/GenericsHomeworkTests/UnitTests.cs
@@ -0,0 +1,72 @@
+using System.Data;
+using GenericsHomework;
+using NUnit.Framework;
+
+#pragma warning disable NUnit2005
+namespace GenericsHomeworkTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void CanCreateNode()
+    {
+        string item = "test";
+        var node = new Node<string>(item);
+        Assert.AreEqual(item, node.Item);
+    }
+
+    [Test]
+    public void ExistsFindsObjects()
+    {
+        string item = "test";
+        var node = new Node<string>(item);
+        Assert.True(node.Exists(item));
+    }
+
+    [Test]
+    public void ThrowsOnDuplicateItemAppended()
+    {
+        string item = "test";
+        string dupe = "test";
+        var node = new Node<string>(item);
+        Assert.Throws<DuplicateNameException>(() => node.Append(dupe));
+    }
+
+    [Test]
+    public void AppendAddsItem()
+    {
+        string itemB = "item_b";
+        var node = new Node<string>("item_a");
+        node.Append(itemB);
+        Assert.AreEqual(node.Next.Item, itemB);
+    }
+
+    [Test]
+    public void ToStringReturnsItemToString()
+    {
+        string item = "test";
+        var node = new Node<string>(item);
+        Assert.AreEqual(item, node.ToString());
+    }
+
+    [Test]
+    public void NextReturnsThisIfNoNext()
+    {
+        var node = new Node<string>("test");
+        Assert.AreEqual(node, node.Next);
+    }
+
+    [Test]
+    public void ClearRemovesExtraNodes()
+    {
+        var node = new Node<string>("item_a");
+        node.Append("item_b");
+        node.Clear();
+        Assert.AreEqual(node, node.Next);
+    }
+}
