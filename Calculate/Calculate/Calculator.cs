f8c114892d522e810f879ab63d0de34b408a77f0
Nolan Posey <nposey1@ewu.edu>
2023-02-19 18:34:16 -0800

    Added MathematicalOperations property and Add, Subtract, Multiply, and Divide methods to Calculator class

--- Calculate/Calculate/Calculator.cs
+++ Calculate/Calculate/Calculator.cs
@@ -8,5 +8,26 @@ namespace Calculate
 {
     public class Calculator
     {
+        public Calculator() 
+        {
+            MathematicalOperations = new Dictionary<char, Func<float, float, float>>();
+        }
+        public IReadOnlyDictionary<char,Func<float,float,float>>? MathematicalOperations { get; }
+        public static float Add(float leftOperand, float rightOperand) 
+        {
+            return leftOperand + rightOperand;
+        }
+        public static float Subtract(float leftOperand, float rightOperand)
+        {
+            return leftOperand - rightOperand;
+        }
+        public static float Multiply(float leftOperand, float rightOperand)
+        {
+            return leftOperand * rightOperand;
+        }
+        public static float Divide(float leftOperand, float rightOperand)
+        {
+            return leftOperand / rightOperand;
+        }
     }
 }
