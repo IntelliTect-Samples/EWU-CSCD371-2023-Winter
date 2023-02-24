bdaaef08e836f92916fb6820d8a98988b3b9543d
BenFristadSchool <bfristad@ewu.edu>
2023-02-23 22:23:06 -0800

    Consolidated PropertyGroup XML to Directory.Build.props, added edge case checking for ensuring input contains 3 strings, and case for dividing by 0

--- Calculate/Calculate/Calculator.cs
+++ Calculate/Calculate/Calculator.cs
@@ -7,7 +7,8 @@ namespace Calculate
         private static readonly Func<int, int, int> Add = (left, right) => left + right;
         private static readonly Func<int, int, int> Subtract = (left, right) => left - right;
         private static readonly Func<int, int, int> Multiply  = (left, right) => left * right;
-        private static readonly Func<int, int, int> Divide  = (left, right) => left / right;
+        private static readonly Func<int, int, int> Divide  = (left, right) => 
+        right.Equals(0) ? left / right : throw new ArgumentException("Cannot Divide by 0");
 
         public IReadOnlyDictionary<char, Func<int, int, int>> MathmaticalOperations = new Dictionary<char, Func<int, int, int>>
         {
@@ -20,6 +21,13 @@ namespace Calculate
         public bool TryCalculate(string expression, out int result)
         {
             string[] expressionArray = expression.Split(" ");
+
+            if (!expressionArray.Length.Equals(3))
+            {
+                result = 0;
+                return false;
+            }
+
             if (int.TryParse(expressionArray[0], out int left) && 
                 char.TryParse(expressionArray[1], out char op) && 
                 int.TryParse(expressionArray[2], out int right))
