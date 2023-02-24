f69e40a73aebfa41d6bb043d012f838285e8e216
Nolan Posey <nposey1@ewu.edu>
2023-02-24 10:00:55 -0800

    Added 4 tests to PersonTests

--- Assignment/Assignment.Tests/PersonTests.cs
+++ Assignment/Assignment.Tests/PersonTests.cs
@@ -7,9 +7,59 @@ namespace Assignment.Tests
     public class PersonTests
     {
         [TestMethod]
-        public void MyTestMethod()
+        public void Person_FirstNamePropertyGetAndSet_AreEqual()
         {
+            //Arrange
+            Address address = new("1234 S Somewhere St", "Atlanta", "GA", "44356");
+            Person person = new("John", "Doe", address, "someGuy@yahoo.com");
 
+            //Act
+            person.FirstName = "Jane";
+
+            //Assert
+            Assert.AreEqual<string>("Jane", person.FirstName);
+        }
+
+        [TestMethod]
+        public void Person_LastNamePropertyGetAndSet_AreEqual()
+        {
+            //Arrange
+            Address address = new("1234 S Somewhere St", "Atlanta", "GA", "44356");
+            Person person = new("John", "Doe", address, "someGuy@yahoo.com");
+
+            //Act
+            person.LastName = "McDonald";
+
+            //Assert
+            Assert.AreEqual<string>("McDonald", person.LastName);
+        }
+
+        [TestMethod]
+        public void Person_AddressPropertyGetAndSet_AreEqual()
+        {
+            //Arrange
+            Address address = new("1234 S Somewhere St", "Atlanta", "GA", "44356");
+            Person person = new("John", "Doe", address, "someGuy@yahoo.com");
+
+            //Act
+            Address newAddress = new("4321 N SomewhereElse Ave", "Portland", "OR", "66574");
+            person.Address = newAddress;
+
+            //Assert
+            Assert.AreEqual<IAddress>(newAddress, person.Address);
+        }
+
+        [TestMethod]
+        public void Person_EmailPropertyGetAndSet_AreEqual()
+        {
+            //Arrange
+            Address address = new("1234 S Somewhere St", "Atlanta", "GA", "44356");
+            Person person = new("John", "Doe", address, "someGuy@yahoo.com");
+
+            //Act
+            person.EmailAddress = "AnotherGuy@Gmail.com";
+            //Assert
+            Assert.AreEqual<string>("AnotherGuy@Gmail.com", person.EmailAddress);
         }
     }
 }
