9a6ef43a516c2e1ceb097ede0869915ba83262f5
George <georgekoralski@hotmail.com>
2023-02-28 00:17:30 -0800

    Implement point 1 and 2

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Assignment.Tests
+{
+    [TestClass()]
+    public class SampleDataTests
+    {
+        [TestMethod()]
+        public void SampleDataTest()
+        {
+            SampleData sampleData = new SampleData();
+            Assert.IsNotNull(sampleData);
+            Assert.AreEqual<int>(50, sampleData.CsvRows.Count());
+            Assert.AreEqual<string>("1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Helena,MT,70577", sampleData.CsvRows.First());
+        }
+
+        [TestMethod()]
+        public void GetUniqueSortedListOfStatesGivenCsvRowsTest()
+        {
+            string expectedStates = "AL,AZ,CA,DC,FL,GA,IN,KS,LA,MD,MN,MO,MT,NC,NE,NH,NV,NY,OR,PA,SC,TN,TX,UT,VA,WA,WV";
+            SampleData sampleData = new SampleData();
+            IEnumerable<string> states = sampleData.GetUniqueSortedListOfStatesGivenCsvRows();
+            Assert.IsNotNull(states);
+            Assert.AreEqual<int>(27, states.Count());
+            // Use hardcoded list to verify result
+            Assert.AreEqual<string>(expectedStates, string.Join(",", states));
+
+            // uses LINQ to verify the data is sorted correctly (do not use a hardcoded list)
+            IEnumerable<string> sortedStates = states.OrderBy(x => x);
+            Assert.AreEqual<string>(string.Join(",", sortedStates), string.Join(",", states));
+        }
+
+        [TestMethod()]
+        public void GetAggregateSortedListOfStatesUsingCsvRowsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FilterByEmailAddressTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAggregateListOfStatesGivenPeopleCollectionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
+        private IEnumerable<string> rows;
+
+        public SampleData() {
+            this.rows = File.ReadAllLines("People.csv").Skip(1);
+        }
+
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        public IEnumerable<string> CsvRows => rows;
 
         // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+        {
+            return CsvRows.Select(x => x.Split(',')[6]).Distinct().OrderBy(x => x);
+        }
 
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
