d8ff51f93dfe18ab4c8ec12223fec6d15e975dae
daisypalencia <dpalencia@ewu.edu>
2023-03-05 19:22:11 -0800

    Changes made to Assignment 7+8 LINQ and unit tests added for each method.

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Assignment;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Assignment.Tests
+{
+	[TestClass]
+	public class SampleDataTests
+	{
+        private readonly SampleData _data = new("/Users/daisypalencia/Desktop/repos/EWU-CSCD371-2023-Winter/Assignment/Assignment/People.csv");
+
+        [TestMethod]
+		public void SampleData_ReadCSV_Success()
+		{
+			SampleData sampleData = _data;
+
+			Assert.IsNotNull(sampleData);
+
+        }
+        [TestMethod]
+        public void SampleData_FillListCsvRows_Success()
+        {
+
+            SampleData sampleData = _data;
+
+            var people = sampleData.People;
+
+            Assert.AreEqual(50, people.Count());
+
+
+        }
+        [TestMethod]
+		public void SampleData_GetUniqueSortedListOfStates_Success()
+		{
+            SampleData sampleData = _data;
+
+            IEnumerable<string> state = sampleData.GetUniqueSortedListOfStatesGivenCsvRows();
+
+			Assert.AreEqual<string>("AL", state.First());
+			Assert.AreEqual<string>("WV", state.Last());
+
+        }
+        [TestMethod]
+        public void SampleData_HardCodedAddresses_Success()
+		{
+
+			List<Address> addresses = new()
+			{ 
+			new Address("9431 Lotheville Pass", "Washington", "WA", "73484"),
+			new Address("662 Rutledge Point", "Spokane", "WA", "99021"),
+			new Address("554 Forster Lane", "Lincoln", "NE", "40053"),
+            };
+
+			IEnumerable<Address> state = addresses.DistinctBy(address => address.State);
+            IEnumerable<Address> zip = addresses.DistinctBy(address => address.Zip);
+            IEnumerable<Address> streetAddress = addresses.DistinctBy(address => address.StreetAddress);
+
+
+            Assert.AreEqual<string>("WA", addresses.ElementAt(0).State);
+			Assert.AreEqual <string> ("WA", addresses.ElementAt(1).State);
+            Assert.AreEqual<string>("NE", addresses.ElementAt(2).State);
+
+            Assert.AreEqual<string>("73484", addresses.ElementAt(0).Zip);
+            Assert.AreEqual<string>("99021", addresses.ElementAt(1).Zip);
+            Assert.AreEqual<string>("40053", addresses.ElementAt(2).Zip);
+
+            Assert.AreEqual<string>("9431 Lotheville Pass", addresses.ElementAt(0).StreetAddress);
+            Assert.AreEqual<string>("662 Rutledge Point", addresses.ElementAt(1).StreetAddress);
+            Assert.AreEqual<string>("554 Forster Lane", addresses.ElementAt(2).StreetAddress);
+
+        }
+        [TestMethod]
+        public void SampleData_GetUniqueSortedListUsingCsvRows_Success()
+        {
+            SampleData sampleData = _data;
+            IEnumerable<string> uniqueSortedListOfStates = _data.GetUniqueSortedListOfStatesGivenCsvRows();
+            Assert.AreEqual<int>(27, uniqueSortedListOfStates.Count());
+        }
+
+        [TestMethod]
+        public void SampleData_ReturnsCorrectCountAndPeople_Success()
+        { 
+            SampleData sampleData = _data;
+
+            var people = sampleData.People;
+
+            Assert.AreEqual(50, people.Count());
+            Assert.IsTrue(people.All(p => p is Person));
+        }
+        [TestMethod]
+        public void SampleData_GetAggregatedListOfStates_Success()
+        {
+            SampleData sampleData = _data;
+
+            IEnumerable<IPerson> people = sampleData.People;
+
+            string result = sampleData.GetAggregateListOfStatesGivenPeopleCollection(people);
+
+            Assert.IsNotNull(result);
+
+        }
+        [TestMethod]
+        public void FilterByEmailAddress()
+        {
+            SampleData sampleData = _data;
+
+            var email = sampleData.FilterByEmailAddress(email => email == "cstennine2@wired.com");
+            var value = email as (string FirstName, string LastName)[] ?? email.ToArray();
+
+            foreach (var person in value)
+            {
+                Assert.IsTrue(person.FirstName == "Chadd");
+            }
+
+            Assert.AreEqual(1, value.Count());
+        }
+    }
+}
+
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,30 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
+        private List<string>? CsvRows1;
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        public SampleData(string filepath)
+        {
+            IEnumerable<string> csv = File.ReadAllLines(filepath);
+            CsvRows = csv;
+        }
+
+        public IEnumerable<string> CsvRows
+        {
+            get
+            {
+                return CsvRows1 ?? throw new ArgumentNullException();
+            }
+            set
+            {
+                CsvRows1 = value.Where(x => x != null).Skip(1).ToList();
+            }
+  
+        }    
 
         // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+        {
+            return CsvRows.Select(row => row.Split(',')[6]).Distinct().OrderBy(state => state).ToList();
 
+        }
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
-            => throw new NotImplementedException();
+        {
+            var uniqueStates = GetUniqueSortedListOfStatesGivenCsvRows().ToArray();
 
+            return string.Join(",", uniqueStates);
+        }
         // 4.
-        public IEnumerable<IPerson> People => throw new NotImplementedException();
+        public IEnumerable<IPerson> People
+        {
+            get
+            {
+                IEnumerable<Person> people = from person in CsvRows
+                .OrderBy(state => state.Split(',')[6])
+                .ThenBy(city => city.Split(',')[5])
+                .ThenBy(zip => zip.Split(',')[7])
+                .ToList()
+
+                let personAttribute = person.Split(',')
+                select new Person(personAttribute[1], personAttribute[2],
+                new Address(personAttribute[4], personAttribute[5], personAttribute[6], personAttribute[7]),
+                personAttribute[3]);
+
+                return people;
+             }
+         }
 
         // 5.
-        public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
-            Predicate<string> filter) => throw new NotImplementedException();
-
+        public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter)
+        {
+            List<IPerson> matches = People.Where(x => filter(x.EmailAddress)).ToList();
+            List<(string, string)> nameList = new();
+            foreach(IPerson item in matches)
+            {
+                nameList.Add((item.FirstName, item.LastName));
+            }
+            return nameList;
+        }
         // 6.
-        public string GetAggregateListOfStatesGivenPeopleCollection(
-            IEnumerable<IPerson> people) => throw new NotImplementedException();
+        public string GetAggregateListOfStatesGivenPeopleCollection(IEnumerable<IPerson> people)
+            => people.Select(person => person.Address.State).Distinct().OrderBy(state => state).Aggregate((all, state) => $"{all}, {state}");
     }
 }
