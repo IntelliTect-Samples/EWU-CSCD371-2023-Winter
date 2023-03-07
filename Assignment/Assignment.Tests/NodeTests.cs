f399c5124acbcf385b2eedd97dfd42120105dfc0
Vera <vkorchemnaya@eagles.ewu.edu>
2023-03-06 21:00:58 -0800

    Node class and tests

---
+++ Assignment/Assignment.Tests/NodeTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Assignment.Tests;
+
+[TestClass]
+public class NodeTests
+{
+    [TestMethod]
+    public void GetEnumerator_ReturnsNodeValues()
+    {
+        Node<int> newNode = new(1);
+        newNode.Append(2).Append(3).Append(4).Append(5).Append(6);
+
+        foreach(int value in newNode)
+        {
+            Console.WriteLine(value);
+        }
+
+        Assert.AreEqual<int>(6, newNode.Count());
+
+    }
+
+    [TestMethod]
+    public void ChildItems_GivenMaximumOfThree_ReturnThreeValues()
+    {
+        Node<int> newNode = new(1);
+        newNode.Append(2).Append(3).Append(4).Append(5).Append(6);
+
+        Assert.AreEqual<int>(3, newNode.ChildItems(3).Count());
+    }
+}
---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Node<T> : IEnumerable<T>
+    {
+        private readonly T _value;
+        private Node<T> _next;
+        public Node(T value)
+        {
+            _value = value;
+            _next = this;
+        }
+
+        public T Value { get { return _value; } }
+
+        public override string? ToString()
+        {
+            if (_value == null) return null;
+            return _value.ToString();
+        }
+
+        public Node<T> Next
+        {
+            get { return _next; }
+            private set
+            {
+                _next = value;
+            }
+        }
+
+        public Node<T> Append(T value)
+        {
+            if (Exists(value)) throw new ArgumentException("This item already exists in the list");
+            Node<T> newNode = new(value);
+            newNode.Next = Next;
+            Next = newNode;
+            return newNode;
+        }
+
+        public bool Exists(T value)
+        {
+            var node = this;
+            do
+            {
+                if (EqualityComparer<T>.Default.Equals(node.Value, value)) return true;
+                node = node.Next;
+            } while (node != this);
+            return false;
+        }
+
+        public void Clear()
+        {
+            var node = Next;
+            while (node.Next != this)
+            {
+                node = node.Next;
+            }
+            node.Next = Next;
+            Next = this;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+
+            Node<T> node = this;
+            do
+            {
+                yield return node.Value;
+                node = node.Next;
+            }
+            while (node != this);
+            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerable<T> ChildItems(int maximum)
+        {
+            Node<T> node = Next;
+            do
+            {
+                yield return node.Value;
+                node = node.Next;
+                maximum--;
+            } while (maximum > 0 && node != this);
+        }
+
+
+    }
+}
