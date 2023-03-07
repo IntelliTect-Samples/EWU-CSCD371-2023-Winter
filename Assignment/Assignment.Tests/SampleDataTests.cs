acb477f6fc7bbeb5b8e13b218b71802c033323bf
AndrewKorchemniy <andrewkorch@gmail.com>
2023-03-06 19:28:27 -0800

    Implenting tests

--- Assignment/Assignment.Tests/SampleDataTests.cs
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -1,129 +1,117 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
-namespace Assignment.Tests
+namespace Assignment.Tests;
+
+[TestClass]
+public class SampleDataTests
 {
-    [TestClass]
-    public class SampleDataTests
+
+    [TestMethod]
+    public void CsvRows_GivenCSV_ReadsAllValidRows()
+    {
+        SampleData data = new();
+
+        Assert.AreEqual<int>(50, data.CsvRows.Count());
+    }
+
+    [TestMethod]
+    public void GetUniqueSortedListOfStatesGivenCsvRows_HardCodedList_Success()
     {
 
-        [TestMethod]
-        public void CsvRows_GetsAllRows_Success()
+        SampleData hardCodedList = new()
         {
-            SampleData data = new();
-
-            Assert.AreEqual<int>(50, data.CsvRows.Count());
-        }
-
-        [TestMethod]
-        public void GetUniqueSortedListOfStatesGivenCsvRows_HardCodedList_Success()
-        {
-
-            SampleData hardCodedList = new()
-            {
-                CsvRows = new List<string> {
-                    "1,First,Last,example@example.com,542 W 27th St,New York,NY,10001",
-                    "1,First,Last,example@example.com,181 Fremont St,San Francisco,CA,94105",
-                    "1,First,Last,example@example.com,55 E Jackson Blvd,Chicago,IL,60604",
-                    "1,First,Last,example@example.com,233 S Wacker Dr,Chicago,IL,60606",
-                    "1,First,Last,example@example.com,333 S Hope St,Los Angeles,CA,90071"
-                }
-            };
-
-            List<string> expected = new() { "CA", "IL", "NY" };
-            IEnumerable<string> actual = hardCodedList.GetUniqueSortedListOfStatesGivenCsvRows();
-
-            static bool ValidateTuple((string First, string Second) tuple) => tuple.First.Equals(tuple.Second);
-            Assert.AreEqual<int>(expected.Count, actual.Zip(expected).Where(ValidateTuple).Count());
-
-        }
-
-        // Not Finished **** 
-
-        [TestMethod]
-        public void GetUniqueSortedListOfStatesGivenCsvRows_Success()
-        {
-            SampleData data = new();
-            /*foreach (string state in data.GetUniqueSortedListOfStatesGivenCsvRows())
-            {
-                Console.WriteLine(state);
-            }*/
-
-
-            Assert.AreEqual<int>(27, data.GetUniqueSortedListOfStatesGivenCsvRows().Count());
-        }
-
-
-        [TestMethod]
-        public void GetAggregateSortedListOfStatesUsingCsvRows_Success()
-        {
-            SampleData data = new();
-            Console.WriteLine(data.GetAggregateSortedListOfStatesUsingCsvRows());
-
-
-        }
-
-        [TestMethod]
-        public void People_RetrieveList_Success()
-        {
-            SampleData data = new()
-            {
-                CsvRows = new List<string> {
-                    "1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Atlanta,AL,70577",
-                    "2,Karin,Joder,kjoder1@quantcast.com,03594 Florence Park,Atlanta,AL,71961",
-                    "3,Chadd,Stennine,cstennine2@wired.com,94148 Kings Terrace,Atlanta,AL,59721",
-                    "4,Fremont,Pallaske,fpallaske3@umich.edu,16958 Forster Crossing,Atlanta,AL,10687",
-                    "5,Melisa,Kerslake,mkerslake4@dion.ne.jp,283 Pawling Parkway,Atlanta,AL,88632",
-                    "6,Darline,Brauner,dbrauner5@biglobe.ne.jp,33 Menomonie Trail,Atlanta,AL,10687"
-                }
-            };
-
-            foreach (Person peep in data.People)
-            {
-                Console.WriteLine(peep.FirstName + " " + peep.LastName + " " + peep.Address.State
-                    + " " + peep.Address.City + " " + peep.Address.StreetAddress);
-
+            CsvRows = new List<string> {
+                "1,First,Last,example@example.com,542 W 27th St,New York,NY,10001",
+                "1,First,Last,example@example.com,181 Fremont St,San Francisco,CA,94105",
+                "1,First,Last,example@example.com,55 E Jackson Blvd,Chicago,IL,60604",
+                "1,First,Last,example@example.com,233 S Wacker Dr,Chicago,IL,60606",
+                "1,First,Last,example@example.com,333 S Hope St,Los Angeles,CA,90071"
             }
-        }
+        };
 
-        [TestMethod]
-        public void FilterByEmailAdreess_HardCodedList_Success()
+        List<string> expected = new() { "CA", "IL", "NY" };
+        IEnumerable<string> actual = hardCodedList.GetUniqueSortedListOfStatesGivenCsvRows();
+
+        static bool ValidateTuple((string First, string Second) tuple) => tuple.First.Equals(tuple.Second);
+        Assert.AreEqual<int>(expected.Count, expected.Zip(actual).Where(ValidateTuple).Count());
+
+    }
+
+    // Not Finished **** 
+
+    [TestMethod]
+    public void GetUniqueSortedListOfStatesGivenCsvRows_Success()
+    {
+        // Arrange
+        SampleData data = new();
+
+        // Act
+        IEnumerable<string> expected = data.CsvRows.Select(item => item.Split(",")[6]).Distinct().OrderBy(state => state);
+        IEnumerable<string> actual = data.GetUniqueSortedListOfStatesGivenCsvRows();
+        static bool ValidateTuple((string First, string Second) tuple) => tuple.First.Equals(tuple.Second);
+
+        // Assert
+        Assert.AreEqual<int>(expected.Count(), expected.Zip(actual).Where(ValidateTuple).Count());
+    }
+
+
+    [TestMethod]
+    public void GetAggregateSortedListOfStatesUsingCsvRows_Success()
+    {
+        SampleData data = new();
+        Console.WriteLine(data.GetAggregateSortedListOfStatesUsingCsvRows());
+
+        // TODO
+    }
+
+    [TestMethod]
+    public void People_GivenCSVList_CreatesPeople()
+    {
+        // Arrange
+        SampleData data = new();
+
+        // Assert
+        Assert.AreEqual<int>(50, data.People.Count());
+    }
+
+    [TestMethod]
+    public void FilterByEmailAdreess_GivenFilter_ReturnsFiltered()
+    {
+        SampleData data = new()
         {
-            SampleData data = new()
-            {
-                CsvRows = new List<string> {
-                    "1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Atlanta,AL,70577",
-                    "2,Karin,Joder,kjoder1@quantcast.com,03594 Florence Park,Atlanta,AL,71961",
-                    "3,Chadd,Stennine,cstennine2@wired.com,94148 Kings Terrace,Atlanta,AL,59721",
-                    "4,Fremont,Pallaske,fpallaske3@state.gov,16958 Forster Crossing,Atlanta,AL,10687",
-                    "5,Melisa,Kerslake,mkerslake4@dion.ne.jp,283 Pawling Parkway,Atlanta,AL,88632",
-                    "6,Darline,Brauner,dbrauner5@biglobe.ne.jp,33 Menomonie Trail,Atlanta,AL,10687"
-                }
-            };
-
-            static bool Filter(string value)
-            {
-                return value.Contains("state.gov");
+            CsvRows = new List<string> {
+                "1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Atlanta,AL,70577",
+                "2,Karin,Joder,kjoder1@quantcast.com,03594 Florence Park,Atlanta,AL,71961",
+                "3,Chadd,Stennine,cstennine2@wired.com,94148 Kings Terrace,Atlanta,AL,59721",
+                "4,Fremont,Pallaske,fpallaske3@state.gov,16958 Forster Crossing,Atlanta,AL,10687",
+                "5,Melisa,Kerslake,mkerslake4@dion.ne.jp,283 Pawling Parkway,Atlanta,AL,88632",
+                "6,Darline,Brauner,dbrauner5@biglobe.ne.jp,33 Menomonie Trail,Atlanta,AL,10687"
             }
+        };
+        List<string> expected = new() { "Fremont Pallaske", "Priscilla Jenyns" };
 
-            List<string> expected = new() { "Fremont Pallaske", "Priscilla Jenyns" };
-            IEnumerable<string> actual = data.FilterByEmailAddress(Filter).Select(person => $"{person.FirstName} {person.LastName}"); ;
+        static bool EmailFilter(string value) => value.Contains(".gov");            
+        IEnumerable<string> actual = data.FilterByEmailAddress(EmailFilter).Select(person => $"{person.FirstName} {person.LastName}");
 
-            static bool ValidateTuple((string Expected, string Actual) tuple) => tuple.Expected.Equals(tuple.Actual);
-            Assert.AreEqual<int>(expected.Count, expected.Zip(actual).Where(ValidateTuple).Count());
-        }
+        static bool ValidateTuple((string Expected, string Actual) tuple) => tuple.Expected.Equals(tuple.Actual);
+        Assert.AreEqual<int>(expected.Count, expected.Zip(actual).Where(ValidateTuple).Count());
+    }
 
-        [TestMethod]
-        public void GetAggregateListOfStatesGivenPeopleCollection_GivenCsvRows_Success()
-        {
-            SampleData data = new();
+    [TestMethod]
+    public void GetAggregateListOfStatesGivenPeopleCollection_GivenCsvRows_Success()
+    {
+        // Arrange
+        SampleData data = new();
 
-            string expected = data.GetUniqueSortedListOfStatesGivenCsvRows().Aggregate((accumulator, states) => $"{accumulator},{states}");
-            string actual = data.GetAggregateListOfStatesGivenPeopleCollection(data.People);
+        // Act
+        string expected = data.GetUniqueSortedListOfStatesGivenCsvRows().Aggregate((accumulator, states) => $"{accumulator},{states}");
+        string actual = data.GetAggregateListOfStatesGivenPeopleCollection(data.People);
 
-            Assert.AreEqual<string>(expected, actual);
-        }
+        // Assert
+        Assert.AreEqual<string>(expected, actual);
     }
 }
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -2,59 +2,57 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Collections;
 
-namespace Assignment
+namespace Assignment;
+
+public class SampleData : ISampleData
 {
-    public class SampleData : ISampleData
+    // 1.
+    public IEnumerable<string> CsvRows { get => _CsvRows; init { _CsvRows = value; } }
+
+    private IEnumerable<string> _CsvRows = File.ReadAllLines("People.csv").Skip(1);
+
+    // 2.
+    public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
     {
-        // 1.
-        public IEnumerable<string> CsvRows { get => _CsvRows; init { _CsvRows = value; } }
+        return CsvRows.Select(row => row.Split(",")[6]).Distinct().OrderBy(state => state);
+    }
 
-        private IEnumerable<string> _CsvRows = File.ReadAllLines("People.csv").Skip(1);
+    // 3.
+    public string GetAggregateSortedListOfStatesUsingCsvRows()
+    {
+        return string.Join(',', GetUniqueSortedListOfStatesGivenCsvRows());
+    }
 
-        // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+    // 4.
+    public IEnumerable<IPerson> People
+    {
+        get
         {
-            return CsvRows.Select(row => row.Split(",")[6]).Distinct().OrderBy(state => state);
-        }
-
-        // 3.
-        public string GetAggregateSortedListOfStatesUsingCsvRows()
-        {
-            return string.Join(',', GetUniqueSortedListOfStatesGivenCsvRows());
-        }
-
-        // 4.
-        public IEnumerable<IPerson> People
-        {
-            get
+            static Person MakePersons(string row)
             {
-                static Person MakePersons(string row)
-                {
-                    string[] elements = row.Split(",");
-                    Address address = new(elements[4], elements[5], elements[6], elements[7]);
-                    return new Person(elements[1], elements[2], address, elements[3]);
-                }
-
-                return CsvRows.Select(MakePersons)
-                    .OrderBy(person => person.Address.State)
-                    .ThenBy(person => person.Address.City)
-                    .ThenBy(person => person.Address.StreetAddress);
+                string[] elements = row.Split(",");
+                Address address = new(elements[4], elements[5], elements[6], elements[7]);
+                return new Person(elements[1], elements[2], address, elements[3]);
             }
-        }
 
-        // 5.
-        public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter)
-        {
-            return People.Where(person => filter(person.EmailAddress))
-                .Select(person => (person.FirstName, person.LastName));
+            return CsvRows.Select(MakePersons)
+                .OrderBy(person => person.Address.State)
+                .ThenBy(person => person.Address.City)
+                .ThenBy(person => person.Address.StreetAddress);
         }
+    }
 
-        // 6.
-        public string GetAggregateListOfStatesGivenPeopleCollection(IEnumerable<IPerson> people)
-        {
-            return People.Select(person => person.Address.State).Distinct().Aggregate((accumulator, state) => $"{accumulator},{state}");
-        }
+    // 5.
+    public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter)
+    {
+        return People.Where(person => filter(person.EmailAddress))
+            .Select(person => (person.FirstName, person.LastName));
+    }
+
+    // 6.
+    public string GetAggregateListOfStatesGivenPeopleCollection(IEnumerable<IPerson> people)
+    {
+        return People.Select(person => person.Address.State).Distinct().Aggregate((accumulator, state) => $"{accumulator},{state}");
     }
 }
