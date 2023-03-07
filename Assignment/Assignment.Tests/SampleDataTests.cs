0ad0c9a57b2987c45fbd94bbdd642801552c4178
AndrewKorchemniy <andrewkorch@gmail.com>
2023-03-06 21:05:26 -0800

    Refactoring SampleDataTests

--- Assignment/Assignment.Tests/SampleDataTests.cs
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 
 namespace Assignment.Tests;
@@ -9,96 +7,100 @@ namespace Assignment.Tests;
 [TestClass]
 public class SampleDataTests
 {
+    private readonly List<string> hardCodedCSVList = new()
+    {
+        "1,Priscilla,Jenyns,pjenyns0@state.gov,542 W 27th St,New York,NY,10001",
+        "2,Karin,Joder,kjoder1@quantcast.com,181 Fremont St,San Francisco,CA,94105",
+        "3,Chadd,Stennine,cstennine2@wired.com,55 E Jackson Blvd,Chicago,IL,60604",
+        "4,Fremont,Pallaske,fpallaske3@state.gov,16958 Forster Crossing,Atlanta,AL,10687",
+        "5,Melisa,Kerslake,mkerslake4@dion.ne.jp,233 S Wacker Dr,Chicago,IL,60606",
+        "6,Darline,Brauner,dbrauner5@biglobe.ne.jp,333 S Hope St,Los Angeles,CA,90071"
+    };
+
+    private bool IsEqual((string expected, string actual) args) => args.expected.Equals(args.actual);
 
     [TestMethod]
     public void CsvRows_GivenCSV_ReadsAllValidRows()
     {
-        SampleData data = new();
+        // Arrange
+        SampleData data = new() { CsvRows = hardCodedCSVList };
 
-        Assert.AreEqual<int>(50, data.CsvRows.Count());
+        // Assert
+        Assert.AreEqual<int>(hardCodedCSVList.Count, data.CsvRows.Count());
     }
 
     [TestMethod]
     public void GetUniqueSortedListOfStatesGivenCsvRows_HardCodedList_Success()
     {
+        // Arrange
+        SampleData data = new() { CsvRows = hardCodedCSVList };
+        List<string> expected = new() { "AL", "CA", "IL", "NY" };
 
-        SampleData hardCodedList = new()
-        {
-            CsvRows = new List<string> {
-                "1,First,Last,example@example.com,542 W 27th St,New York,NY,10001",
-                "1,First,Last,example@example.com,181 Fremont St,San Francisco,CA,94105",
-                "1,First,Last,example@example.com,55 E Jackson Blvd,Chicago,IL,60604",
-                "1,First,Last,example@example.com,233 S Wacker Dr,Chicago,IL,60606",
-                "1,First,Last,example@example.com,333 S Hope St,Los Angeles,CA,90071"
-            }
-        };
+        // Act
+        IEnumerable<string> actual = data.GetUniqueSortedListOfStatesGivenCsvRows();
 
-        List<string> expected = new() { "CA", "IL", "NY" };
-        IEnumerable<string> actual = hardCodedList.GetUniqueSortedListOfStatesGivenCsvRows();
-
-        static bool ValidateTuple((string First, string Second) tuple) => tuple.First.Equals(tuple.Second);
-        Assert.AreEqual<int>(expected.Count, expected.Zip(actual).Where(ValidateTuple).Count());
-
+        // Assert
+        Assert.AreEqual<int>(expected.Count, expected.Zip(actual).Where(IsEqual).Count());
     }
-
-    // Not Finished **** 
 
     [TestMethod]
     public void GetUniqueSortedListOfStatesGivenCsvRows_Success()
     {
         // Arrange
         SampleData data = new();
+        IEnumerable<string> expected = data.CsvRows.Select(item => item.Split(",")[6]).Distinct().OrderBy(state => state);
 
         // Act
-        IEnumerable<string> expected = data.CsvRows.Select(item => item.Split(",")[6]).Distinct().OrderBy(state => state);
         IEnumerable<string> actual = data.GetUniqueSortedListOfStatesGivenCsvRows();
-        static bool ValidateTuple((string First, string Second) tuple) => tuple.First.Equals(tuple.Second);
 
         // Assert
-        Assert.AreEqual<int>(expected.Count(), expected.Zip(actual).Where(ValidateTuple).Count());
+        Assert.AreEqual<int>(expected.Count(), expected.Zip(actual).Where(IsEqual).Count());
     }
 
 
     [TestMethod]
     public void GetAggregateSortedListOfStatesUsingCsvRows_Success()
     {
-        SampleData data = new();
-        Console.WriteLine(data.GetAggregateSortedListOfStatesUsingCsvRows());
+        // Arrange
+        SampleData data = new() { CsvRows = hardCodedCSVList };
+        string expected = "AL,CA,IL,NY";
 
-        // TODO
+        // Act
+        string actual = data.GetAggregateSortedListOfStatesUsingCsvRows();
+
+        // Assert
+        Assert.AreEqual<string>(expected, actual);
     }
 
     [TestMethod]
     public void People_GivenCSVList_CreatesPeople()
     {
         // Arrange
-        SampleData data = new();
+        SampleData data = new() { CsvRows = hardCodedCSVList };
 
         // Assert
-        Assert.AreEqual<int>(50, data.People.Count());
+        Assert.AreEqual<int>(6, data.People.Count());
+        Assert.AreEqual<string>("Fremont", data.People.First().FirstName);
+        Assert.AreEqual<string>("Pallaske", data.People.First().LastName);
+        Assert.AreEqual<string>("fpallaske3@state.gov", data.People.First().EmailAddress);
+        Assert.AreEqual<string>("AL", data.People.First().Address.State);
+        Assert.AreEqual<string>("Atlanta", data.People.First().Address.City);
+        Assert.AreEqual<string>("16958 Forster Crossing", data.People.First().Address.StreetAddress);
     }
 
     [TestMethod]
     public void FilterByEmailAdreess_GivenFilter_ReturnsFiltered()
     {
-        SampleData data = new()
-        {
-            CsvRows = new List<string> {
-                "1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Atlanta,AL,70577",
-                "2,Karin,Joder,kjoder1@quantcast.com,03594 Florence Park,Atlanta,AL,71961",
-                "3,Chadd,Stennine,cstennine2@wired.com,94148 Kings Terrace,Atlanta,AL,59721",
-                "4,Fremont,Pallaske,fpallaske3@state.gov,16958 Forster Crossing,Atlanta,AL,10687",
-                "5,Melisa,Kerslake,mkerslake4@dion.ne.jp,283 Pawling Parkway,Atlanta,AL,88632",
-                "6,Darline,Brauner,dbrauner5@biglobe.ne.jp,33 Menomonie Trail,Atlanta,AL,10687"
-            }
-        };
+        // Arrange
+        SampleData data = new() { CsvRows = hardCodedCSVList };
         List<string> expected = new() { "Fremont Pallaske", "Priscilla Jenyns" };
 
-        static bool EmailFilter(string value) => value.Contains(".gov");            
+        // Act
         IEnumerable<string> actual = data.FilterByEmailAddress(EmailFilter).Select(person => $"{person.FirstName} {person.LastName}");
 
-        static bool ValidateTuple((string Expected, string Actual) tuple) => tuple.Expected.Equals(tuple.Actual);
-        Assert.AreEqual<int>(expected.Count, expected.Zip(actual).Where(ValidateTuple).Count());
+        // Assert
+        static bool EmailFilter(string value) => value.Contains(".gov");
+        Assert.AreEqual<int>(expected.Count, expected.Zip(actual).Where(IsEqual).Count());
     }
 
     [TestMethod]
