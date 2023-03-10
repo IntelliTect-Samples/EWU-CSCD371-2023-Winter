5d482dabdae506dd0fe942ffcb1adffa93073994
daisypalencia <dpalencia@ewu.edu>
2023-03-09 21:10:48 -0800

    changes made to tests.

--- Assignment/Assignment.Tests/SampleDataTests.cs
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -12,14 +12,26 @@ namespace Assignment.Tests
 	public class SampleDataTests
 	{
         private readonly SampleData _data = new("./People.csv");
+        private readonly SampleData _dataSpokane = new("./spokaneaddresses.csv");
 
         [TestMethod]
 		public void SampleData_ReadCSV_Success()
 		{
 			SampleData sampleData = _data;
+            SampleData spokaneData = _dataSpokane;
 
-			Assert.IsNotNull(sampleData);
+            Assert.IsNotNull(sampleData);
+            Assert.IsNotNull(spokaneData);
 
+        }
+        [TestMethod]
+        public void SampleData_SkipsFirstLine_Success()
+        {
+            SampleData sampleData = _data;
+
+            int actual = sampleData.CsvRows.Count();
+            int expected = 50;
+            Assert.AreEqual<int>(expected, actual);
         }
         [TestMethod]
         public void SampleData_FillListCsvRows_Success()
@@ -30,8 +42,6 @@ namespace Assignment.Tests
             var people = sampleData.People;
 
             Assert.AreEqual(50, people.Count());
-
-
         }
         [TestMethod]
 		public void SampleData_GetUniqueSortedListOfStates_Success()
@@ -45,33 +55,14 @@ namespace Assignment.Tests
 
         }
         [TestMethod]
-        public void SampleData_HardCodedAddresses_Success()
-		{
+        public void SampleData_GetUniqueStatesSpokaneAddresses_Success()
+        {
+            SampleData sampleData = _dataSpokane;
+            IEnumerable<string> states = sampleData.GetUniqueSortedListOfStatesGivenCsvRows();
 
-			List<Address> addresses = new()
-			{ 
-			new Address("9431 Lotheville Pass", "Washington", "WA", "73484"),
-			new Address("662 Rutledge Point", "Spokane", "WA", "99021"),
-			new Address("554 Forster Lane", "Lincoln", "NE", "40053"),
-            };
-
-			IEnumerable<Address> state = addresses.DistinctBy(address => address.State);
-            IEnumerable<Address> zip = addresses.DistinctBy(address => address.Zip);
-            IEnumerable<Address> streetAddress = addresses.DistinctBy(address => address.StreetAddress);
-
-
-            Assert.AreEqual<string>("WA", addresses.ElementAt(0).State);
-			Assert.AreEqual <string> ("WA", addresses.ElementAt(1).State);
-            Assert.AreEqual<string>("NE", addresses.ElementAt(2).State);
-
-            Assert.AreEqual<string>("73484", addresses.ElementAt(0).Zip);
-            Assert.AreEqual<string>("99021", addresses.ElementAt(1).Zip);
-            Assert.AreEqual<string>("40053", addresses.ElementAt(2).Zip);
-
-            Assert.AreEqual<string>("9431 Lotheville Pass", addresses.ElementAt(0).StreetAddress);
-            Assert.AreEqual<string>("662 Rutledge Point", addresses.ElementAt(1).StreetAddress);
-            Assert.AreEqual<string>("554 Forster Lane", addresses.ElementAt(2).StreetAddress);
-
+            int actual = 1;
+            int expected = states.Count();
+            Assert.AreEqual<int>(expected, actual);
         }
         [TestMethod]
         public void SampleData_GetUniqueSortedListUsingCsvRows_Success()
@@ -118,6 +109,42 @@ namespace Assignment.Tests
 
             Assert.AreEqual(1, value.Count());
         }
+        [TestMethod]
+        public void People_ReturnsValidListOfIPerson()
+        {
+            SampleData data = new("./People.csv");
+            IEnumerable<string> rowsOrdered = data.CsvRows
+                .OrderBy(row => row.Split(',')[6])
+                .ThenBy(row => row.Split(',')[5])
+                .ThenBy(row => row.Split(',')[7]);
+            IEnumerable<IPerson> people = data.People;
+
+            var zip = rowsOrdered.Zip(people, (row, person) =>
+            {
+                string[] split = row.Split(",");
+                Assert.AreEqual(split[1], person.FirstName);
+                Assert.AreEqual(split[2], person.LastName);
+                Assert.AreEqual(split[3], person.EmailAddress);
+                Assert.AreEqual(split[4], person.Address.StreetAddress);
+                Assert.AreEqual(split[5], person.Address.City);
+                Assert.AreEqual(split[6], person.Address.State);
+                Assert.AreEqual(split[7], person.Address.Zip);
+                return true;
+            });
+
+            Assert.AreEqual(rowsOrdered.Count(), zip.Count());
+        }
+        [TestMethod]
+        public void GetUniqueSortedListOfStatesGivenCsvRows_IsSorted()
+        {
+            SampleData data = new SampleData("./People.csv");
+
+            IEnumerable<string> states = data.GetUniqueSortedListOfStatesGivenCsvRows();
+            List<string> sortedStates = states.OrderBy(s => s).ToList();
+
+            CollectionAssert.AreEqual(sortedStates, (System.Collections.ICollection)states);
+        }
+
     }
 }
 
