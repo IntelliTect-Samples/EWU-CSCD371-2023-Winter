9b47c5ea2b184d4ba4393dd9d69d60d9162a818f
Gunnar Knutson <gknutson1@ewu.edu>
2023-03-09 21:56:23 -0800

    reset project

--- Assignment/Assignment.Interfaces/IAsyncSampleData.cs
+++ Assignment/Assignment.Interfaces/IAsyncSampleData.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace Assignment;
 
-namespace Assignment
+public interface IAsyncSampleData
 {
-    public interface IAsyncSampleData
-    {
-        // 1.
-        IAsyncEnumerable<string> CsvRows { get; }
-        
-        // 2.
-        IAsyncEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows();
+    // 1.
+    IAsyncEnumerable<string> CsvRows { get; }
 
-        // 3.
-        string GetAggregateSortedListOfStatesUsingCsvRows();
+    // 4.
+    IAsyncEnumerable<IPerson> People { get; }
 
-        // 4.
-        IAsyncEnumerable<IPerson> People { get; }
+    // 2.
+    IAsyncEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows();
 
-        // 5.
-        IAsyncEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter);
+    // 3.
+    string GetAggregateSortedListOfStatesUsingCsvRows();
 
-        // 6.
-        string GetAggregateListOfStatesGivenPeopleCollection(IAsyncEnumerable<IPerson> people);
+    // 5.
+    IAsyncEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter);
 
-    }
+    // 6.
+    string GetAggregateListOfStatesGivenPeopleCollection(IAsyncEnumerable<IPerson> people);
 }
